77bb6b0763c4e83ebc65e218dd4037eee093a7ed
sayazian <sahar.ayazian@gmail.com>
2026-06-23 15:48:43 -0700

    queues

--- src/problems/problem26/Queue.java
+++ src/problems/problem26/Queue.java
@@ -1,5 +1,7 @@
 package problems.problem26;
 
+import java.util.NoSuchElementException;
+
 public class Queue<T> {
     Node<T> head;
     Node<T> tail;
@@ -7,36 +9,53 @@ public class Queue<T> {
     static class Node<T> {
         T value;
         Node<T> next;
+
+        Node() {
+        }
+
+        Node(T value) {
+            this.value = value;
+        }
     }
 
     public void enqueue(T value) {
+        Node<T> node = new Node<>(value);
         if (head == null) {
-            head = new Node<T>();
-            head.value = value;
-            tail = head;
+            head = node;
+        } else if (head.next == null) {
+            tail = node;
             head.next = tail;
         } else {
-            Node<T> node = new Node<>();
-            node.value = value;
-            node.next = head;
-            head = node;
-            tail.next = head;
+            tail.next = node;
+            tail = node;
         }
     }
 
-    public T dequeue(){
-        return null;
+    public T dequeue() throws NoSuchElementException {
+        throwNoSuchElementException(head == null);
+        Node<T> node = head.next;
+        T returnValue = head.value;
+        head = node;
+        if (head == tail) {
+            tail = null;
+        }
+        return returnValue;
     }
 
-    public T peek() throws NullPointerException {
-        if (head == null) {
-            throw new NullPointerException();
-        }
+    public T peek() throws NoSuchElementException {
+        throwNoSuchElementException(head == null);
         return head.value;
     }
 
     public boolean isEmpty() {
-        return false;
+        return (head == null);
     }
+
+    private void throwNoSuchElementException(boolean condition) {
+        if (condition) {
+            throw new NoSuchElementException();
+        }
+    }
+
 }
 
--- src/problems/problem26/QueueTest.java
+++ src/problems/problem26/QueueTest.java
@@ -2,25 +2,74 @@ package problems.problem26;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.NoSuchElementException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class QueueTest {
 
     @Test
     void enqueue() {
-        Queue<Integer> q = new Queue<>();
+        Queue<Integer> q = Framework.queueLengthZero();
+        assertThrows(NoSuchElementException.class, () -> {q.dequeue();});
         q.enqueue(1);
+        assertEquals(1,q.peek());
+        q.enqueue(2);
         assertEquals(1,q.peek());
     }
 
     @Test
-    void dequeue() {
+    void dequeue0() {
+        Queue<Integer> q = Framework.queueLengthZero();
+        assertThrows(NoSuchElementException.class, () -> {q.dequeue();});
+    }
+
+    @Test
+    void dequeue1() {
+        Queue<Integer> q = Framework.queueLengthOne();
+        assertEquals(1, q.dequeue());
+        assertThrows(NoSuchElementException.class, () -> {q.peek();});
+        assertTrue(q.isEmpty());
+    }
+
+    @Test
+    void dequeue2() {
+        Queue<Integer> q = Framework.queueLengthTwo();
+        assertEquals(1, q.dequeue());
+        assertEquals(2, q.peek());
+        assertEquals(2, q.dequeue());
+        assertThrows(NoSuchElementException.class, () -> {q.peek();});
+        assertThrows(NoSuchElementException.class, () -> {q.dequeue();});
+        assertTrue(q.isEmpty());
+    }
+
+    @Test
+    void dequeue3() {
+        Queue<Integer> q = Framework.queueLengthThree();
+        q.enqueue(4);
+        q.enqueue(5);
+        q.enqueue(6);
+        assertEquals(1, q.peek());
+        assertEquals(1, q.dequeue());
+        assertEquals(2, q.peek());
+        assertEquals(2, q.dequeue());
+        assertEquals(3, q.peek());
+        assertEquals(3, q.dequeue());
+        assertEquals(4, q.peek());
+        assertEquals(4, q.dequeue());
+        assertEquals(5, q.peek());
+        assertEquals(5, q.dequeue());
+        assertEquals(6, q.peek());
+        assertEquals(6, q.dequeue());
+        assertThrows(NoSuchElementException.class, q::peek);
+        assertThrows(NoSuchElementException.class, q::dequeue);
+        assertTrue(q.isEmpty());
     }
 
     @Test
     void testPeek0() {
         Queue<Integer> q = Framework.queueLengthZero();
-        assertThrows(NullPointerException.class, q::peek);
+        assertThrows(NoSuchElementException.class, q::peek);
     }
 
     @Test
@@ -32,11 +81,13 @@ class QueueTest {
     @Test
     void testPeek2() {
         Queue<Integer> q = Framework.queueLengthTwo();
-        assertEquals(2, q.peek());
+        assertEquals(1, q.peek());
     }
 
     @Test
-    void isEmpty() {
+    void testIsEmpty() {
+        Queue<Integer> q = Framework.queueLengthZero();
+        assertTrue(q.isEmpty());
     }
 
     static class Framework {
